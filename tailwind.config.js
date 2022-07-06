364c31c4f409ca8c6909e285ee4740745db7bc04
Hossein Mazhar <hosseinmazhar1053@gmail.com>
2022-07-06 15:27:33 +0430

    fix git pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./src/style.css" , "./src/index.html" , "./src/app.js"],
+  content: ["./docs/style.css" , "./docs/index.html" , "./docs/app.js"],
   theme: {
     extend: {},
     backgroundImage: {
